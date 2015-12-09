3fca81f3b90346cb0a0d52049781b5aaec7f742a
Devin Chen <d3vin.chen@gmail.com>
2015-12-09 14:12:20 +0800

    Not warning night restart nginx

--- nagios_nrpe/usr/lib64/nagios/plugins/nginx_service.sh
+++ nagios_nrpe/usr/lib64/nagios/plugins/nginx_service.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-RET_OK=0
-RET_WR=1
-RET_CR=2
-RET_UK=3
+# Source function library.
+. /usr/lib64/nagios/plugins/functions
+
+FLAG="/tmp/nginx_is_restarting_now"
+
+# Nginx is restarted every night.
+# Do not warning this job.
+if [ -f $FLAG ]; then
+  echo $FLAG
+  exit $RET_OK
+fi
 
 /etc/init.d/nginx status &> /dev/null
-RET=$?
+nginx_status=$?
 
-if [ $RET -eq 3 ]; then
+if [ $nginx_status -eq 3 ]; then
   echo "Nginx service is stopped, please start nginx, or add to chkconfig."
   exit $RET_CR
 fi
 
-if [ $RET -ne 0 ]; then
-  echo "Something is wrong with nginx, exit code is $RET"
+if [ $nginx_status -ne 0 ]; then
+  echo "Something is wrong with nginx, exit code is $nginx_status"
   exit $RET_WR
 fi
 
